579c7fe8b5cfca1b07967e3af9f28fa5456de903
CarloChiodaroli <carlo.chiodaroli@mail.polimi.it>
2022-04-25 12:49:24 +0200

    Added Filters

---
+++ src/main/java/it/polimi/tiw/tiw2022chioda/filters/EmployeeChecker.java
@@ -0,0 +1,33 @@
+package it.polimi.tiw.tiw2022chioda.filters;
+
+import it.polimi.tiw.tiw2022chioda.bean.User;
+import it.polimi.tiw.tiw2022chioda.enums.UserType;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class EmployeeChecker implements Filter {
+
+    public EmployeeChecker() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println("Employee filter executing");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String loginPath = httpServletRequest.getServletContext().getContextPath() + "/index.html";
+        HttpSession session = httpServletRequest.getSession();
+        User user;
+        user = (User) session.getAttribute("user");
+        if (!user.getUserType().equals(UserType.EMPLOYEE)) {
+            httpServletResponse.sendRedirect(loginPath);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/it/polimi/tiw/tiw2022chioda/filters/LoginChecker.java
@@ -0,0 +1,30 @@
+package it.polimi.tiw.tiw2022chioda.filters;
+
+import it.polimi.tiw.tiw2022chioda.bean.User;
+import it.polimi.tiw.tiw2022chioda.enums.UserType;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginChecker implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println("Checking Login");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String loginPath = httpServletRequest.getServletContext().getContextPath() + "/index.html";
+        HttpSession session = httpServletRequest.getSession();
+        if(session.isNew() || session.getAttribute("user") == null){
+            System.err.println("There is no Login");
+            httpServletResponse.sendRedirect(loginPath);
+            return;
+        }
+        System.out.println("Login ok");
+        chain.doFilter(request, response);
+    }
+}
